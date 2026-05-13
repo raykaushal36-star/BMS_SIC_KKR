043fd21e95b7a1a8530ffb7d8859366b292f6e2a
raykaushal36-star <raykaushal36@gmail.com>
2026-05-13 23:42:18 +0530

    This is for merge sort

--- selection_demo.py
+++ selection_demo.py
@@ -1,4 +1,6 @@
+
 from selection_sort import selection_sort
+import selection_sort as s_sort
 
 import sys
 
@@ -15,3 +17,14 @@ print(f'Numbers before sorting',numbers)
 x=selection_sort(numbers)
 
 print(f'numbers after sorting',x)
+
+for i in range(1,len(sys.argv)):
+    numbers.append(float(sys.argv[i]))
+
+print(f'Number before sorting\n{numbers}')
+
+x=s_sort.selection_sort(numbers)
+
+print(f'Number after sorting\n{numbers}')
+
+
